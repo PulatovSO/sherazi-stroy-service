b9db8b918f6f727966d72defbde8b650ce9b67fe
So coding <pulatovsirotillo@gmail.com>
2022-06-03 13:15:18 +0500

    new designed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
+let menuBtns = document.querySelectorAll('.header__menu-btn');
+let menu = document.querySelector('.menu');
+
+menuBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    if(btn.classList.contains('menu-open')) {
+      menu.style.transform = 'translateX(0)'  
+    } else {
+      menu.style.transform = 'translateX(100%)'  
+    }
+  })
+})
+
 
 /*=============== SLICK SLIDER ===============*/
 $(document).ready(function(){
@@ -10,8 +23,15 @@ $(document).ready(function(){
             {
               breakpoint: 1024,
               settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
               }
             }
         ]
